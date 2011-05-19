fe4821fb6da1db889d83c3c6e2034d794bbca10a
Ian Beveridge <ian.beveridge@gmail.com>
2011-05-19 21:19:25 +0100

    Basic JS

---
+++ js/svgometer-1.0.js
@@ -0,0 +1,17 @@
+/**
+* SVGometer: SVG-based general purpose shiny meter.
+* Author: Ian Beveridge
+* Email: ian.beveridge@gmail.com
+* URL: http://kupio.com/svgometer
+* Version: 1.0
+*/
+
+var SVGometer = (function() { /* Begin class definition */
+	
+	function SVGometer(jqElement)
+	{
+		jqElement.empty();
+		var meter = jQuery("Hello world");
+		jqElement.append(meter);
+	}
+} /* End class definition */
