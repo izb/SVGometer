f15d7303b8555560e6c49d730a5a746cbb0cf07f
Ian Beveridge <ian.beveridge@gmail.com>
2011-05-20 22:39:14 +0100

    Now rotates the needle, to prove I can actually move the damned thing.

--- js/svgometer-1.0.js
+++ js/svgometer-1.0.js
@@ -10,7 +10,12 @@ var SVGometer = (function() { /* Begin class definition */
 	
 	function SVGometer(jqElement, url)
 	{
-		jqElement.load(url);
+		jqElement.load(url, function() {
+			document.getElementById("needle").setAttribute("transform", "translate(100,100) rotate(20)");
+			document.getElementById("redzone").setAttribute("transform", "translate(100,100) rotate(0)");
+			document.getElementById("greenzone").setAttribute("transform", "translate(100,100) rotate(0)");
+			document.getElementById("markers").setAttribute("transform", "translate(100,100) rotate(0)");
+		});
 	};
 	
 return SVGometer;})(); /* End class definition */
