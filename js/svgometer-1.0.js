d20a9909f4f07aef573ce780933628d511109432
Ian Beveridge <ian.beveridge@gmail.com>
2011-05-23 22:37:40 +0100

    Vague attempt at redefining an arc

--- js/svgometer-1.0.js
+++ js/svgometer-1.0.js
@@ -15,7 +15,22 @@ var SVGometer = (function() { /* Begin class definition */
 			document.getElementById("greenlevel").setAttribute("transform", "translate(100,100)");
 			document.getElementById("redlevel").setAttribute("transform", "translate(100,100)");
 			document.getElementById("markers").setAttribute("transform", "translate(100,100) rotate(0)");
+			
+			setRange(document.getElementById("greenpath"), 80, 100);
 		});
+		
+		function setRange(ele, from, to)
+		{
+			var startx = -101.09433; /* TODO: parse the existing path data */
+			var starty = 40.768728;
+			
+			var c = (Math.cos(from) * 80) + startx - 64.25;
+			var s = (Math.sin(from) * 80) + starty - 64.25;
+			
+			console.log("m "+startx+","+starty+" a 64.25,64.25 0 1 1 "+c+","+s);
+			
+			ele.setAttribute("d", "m "+startx+","+starty+" a 64.25,64.25 0 1 1 "+c+","+s);
+		}
 	};
 	
 return SVGometer;})(); /* End class definition */
