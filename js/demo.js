3ebdb0fde4c43b3d3adf724e174af7a8b26794fc
Ian Beveridge <ian.beveridge@gmail.com>
2011-06-01 14:07:16 +0100

    Range and needle pointing works

--- js/demo.js
+++ js/demo.js
@@ -2,6 +2,11 @@ $(document).ready(function () {
 	new SVGometer($("#placement"), "images/guage.svg", {
 		min: 0,
 		max: 1500,
-		label: "This is a test"
+		label: "This is a test",
+		greenmin: 0,
+		greenmax: 500,
+		redmin: 1000,
+		redmax: 1500,
+		initial: 500
 	});		
 });
--- js/svgometer-1.0.js
+++ js/svgometer-1.0.js
@@ -15,27 +15,24 @@ var SVGometer = (function() { /* Begin class definition */
 	 *     min: The minimum value for the meter	
 	 *     max: The maximum value for the meter	
 	 *     label: The label for the meter	
+	 *     greenmin: }
+	 *     greenmax: } Indicator for good/bad values on the meter.
+	 *     redmin:   } Overlap these to create a yellow area
+	 *     redmax:   }
+	 * 	   initial: The initial meter value
 	 * }
 	 * 
 	 */
 	
 	function SVGometer($ele, url, config)
 	{
-		$ele.load(url, function() {
-			
-			$('#txtMin').text(""+config.min);
-			$('#txtMax').text(""+config.max);
-			$('#txtTitle').text(config.label);
-			
-			document.getElementById("needle").setAttribute("transform", "translate(100,100) rotate(20)");
-			document.getElementById("markers").setAttribute("transform", "translate(100,100) rotate(0)");
-			
-			setRange(document.getElementById("redpath"), 0, 270);
-			setRange(document.getElementById("greenpath"), 10, 90);
-		});
+		var meter = this;
 		
-		function setRange(ele, from, to)
+		this.setRange = function(ele, from, to)
 		{
+			from *= this.factor;
+			to *= this.factor;
+			
 			var longarc = (to - from >= 180)? 1:0;
 			
 			from += 135;
@@ -53,6 +50,39 @@ var SVGometer = (function() { /* Begin class definition */
 			
 			console.log(ele);
 		}
+		
+		$ele.load(url, function() {
+			
+			$('#txtMin').text(""+config.min);
+			$('#txtMax').text(""+config.max);
+			$('#txtTitle').text(config.label);
+			
+			meter.min = config.min;
+			meter.max = config.max;
+			
+			meter.greenmin = Math.max(config.min, config.greenmin);
+			meter.greenmax = Math.min(config.max, config.greenmax);
+			meter.redmin = Math.max(config.min, config.redmin);
+			meter.redmax = Math.min(config.max, config.redmax);
+			
+			document.getElementById("markers").setAttribute("transform", "translate(100,100) rotate(0)");
+			
+			var range = config.max - config.min;
+			
+			meter.factor = 270/range;
+			
+			meter.setRange(document.getElementById("redpath"), meter.redmin, meter.redmax);
+			meter.setRange(document.getElementById("greenpath"), meter.greenmin, meter.greenmax);
+			
+			meter.setValue(config.initial);
+		});
 	};
+	
+	SVGometer.prototype.setValue = function(val) {
+		val = Math.max(val, this.min);
+		val = Math.min(val, this.max);
+		/* TODO: Set the text of the needle value too */
+		document.getElementById("needle").setAttribute("transform", "translate(100,100) rotate("+(val * this.factor - 135)+")");
+	}
 	
 return SVGometer;})(); /* End class definition */
